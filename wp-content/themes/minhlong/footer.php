2f3dcca0f54c483c486ec20d480e111b900713ad
Your Name <toanktv.it@gmail.com>
2016-12-21 22:44:16 +0700

    Update dynamic setting backend

--- wp-content/themes/minhlong/footer.php
+++ wp-content/themes/minhlong/footer.php
@@ -8,7 +8,7 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-4 col-md-4">
-                <a href="<?php echo get_bloginfo( 'url' ); ?>"><img class="logo-footer" src="<?php echo get_template_directory_uri();?>/images/logo.png"/></a>
+                <a href="<?php echo get_bloginfo( 'url' ); ?>"><img class="logo-footer" src="<?php echo get_template_directory_uri();?>/images/<?php echo get_option('logoFile');?>"/></a>
                 <p class="copyright"> ©copyright <?php echo date('Y'); ?> <a href="<?php echo get_bloginfo( 'url' ); ?>"><b class="red"> <?php bloginfo( 'sitename' ); ?></b></a></p>
             </div>
             <div class="col-xs-12 col-sm-4 col-md-5">
@@ -26,7 +26,6 @@
     </div>
 </footer>
 <!-- footer -->
-
 <a href="#" class="scrollup">Scroll</a>
 <?php wp_footer(); ?>
 </body>
